5b5cff077e1e885c8b016f02de16bb8fd1e07a34
Loic Maury <lmaury@gmail.com>
2014-03-07 02:27:19 +0100

    modified:   launchd.mk

--- launchd.mk
+++ launchd.mk
@@ -6,6 +6,13 @@ CFLAGS+=-g -fPIC -fno-exceptions -fstack-protector -fvisibility=hidden
 
 CC_COMPILER!= ${CC} --version
 
+# Detect what compilers is used.
+# Two  issues happens here.
+# 1- the use of -Wno-deprecated with gcc, is only for C++ not for C,
+# so detect the compiler used.
+# 2- the use of pragma and -Wall, with gcc <= 4.7.2, cannot be used.
+# in the futur, the version of gcc should be detected,
+# (for use of -Wall).
 .if !empty(CC_COMPILER:M*clang*)
 CFLAGS+=-Wall -Wno-deprecated
 .endif
