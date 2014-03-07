dbb1b0f1e869b2cce91c46446e31dfb2f2a388d9
Loic Maury <lmaury@gmail.com>
2014-03-07 01:58:04 +0100

    Resolve cc1: warning: command line option "-Wno-deprecated" is valid for C++/ObjC++ but not for C and warning: ignoring #pragma message (note we can use -Wall with #pragma with gcc >= 4.7.2) 	modified:   launchd.mk

--- launchd.mk
+++ launchd.mk
@@ -4,11 +4,17 @@ MAINTAINER=tyler@freebsd.org
 
 CFLAGS+=-g -fPIC -fno-exceptions -fstack-protector -fvisibility=hidden
 
+CC_COMPILER!= ${CC} --version
+
+.if !empty(CC_COMPILER:M*clang*)
+CFLAGS+=-Wall -Wno-deprecated
+.endif
+
 # WARNINGS
-CFLAGS+=-Wall -Wextra -Wno-unused-parameter \
+CFLAGS+=-Wextra -Wno-unused-parameter \
 		-Wno-unused-label -Wformat -Wreturn-type \
 		-Wsign-compare -Wmultichar -Winit-self \
-		-Wuninitialized -Wno-deprecated -Wformat-security \
+		-Wuninitialized -Wformat-security \
 		-Werror
 
 # INCLUDES
